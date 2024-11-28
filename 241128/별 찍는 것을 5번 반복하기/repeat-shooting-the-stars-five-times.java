00a626be605d9ff47508d11e2568cf460f20068e
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-11-28 21:18:52 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

--- 241128/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
+++ 241128/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -1,15 +1,14 @@
 public class Main {
-    public void print10Stars() {
+    public static void print10Stars() {
         for(int i = 0; i < 10; i++) {
             System.out.print("*");
         }
         System.out.println();
     }
     public static void main(String[] args) {
-        Main T = new Main();
         for(int i = 0; i < 5; i++) {
             
-            T.print10Stars();
+            print10Stars();
         }
     }
 }
