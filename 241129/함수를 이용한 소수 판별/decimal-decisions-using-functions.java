592341cfc4cb1cc771a71cb8e843bdafceb6cd6f
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-11-29 02:13:39 +0900

    Title: 함수를 이용한 소수 판별, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241129/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime(int n) {
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) { 
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void sum(int a, int b) {
+        int sum = 0;
+        for(int i = a; i <= b; i++) {
+            if(isPrime(i)) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sum(a, b);
+    }
+}
