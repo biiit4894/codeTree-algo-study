7823ef347e1a1214fbfa561829c4932f15c6b5e9
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-11-28 23:07:24 +0900

    Title: 최소공배수 구하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241128/최소공배수 구하기/find-the-least-common-multiple.java
+++ 241128/최소공배수 구하기/find-the-least-common-multiple.java
@@ -8,11 +8,12 @@ public class Main {
         for(int i = 1; i <= Math.min(n, m); i++) {
             if(n % i == 0 && m % i == 0) {
                 gcd = i; 
-                a = n / i; // n을 최대공약수로 나눈 값
-                b = m / i; // m을 최대공약수로 나눈 값
+                // a = n / i; // n을 최대공약수로 나눈 값
+                // b = m / i; // m을 최대공약수로 나눈 값
             }
         }
-        gcm = gcd * a * b; // 최대공약수 * (n을 최대공약수로 나눈 값) * (m을 최대공약수로 나눈 값)
+        // gcm = gcd * a * b; // 최대공약수 * (n을 최대공약수로 나눈 값) * (m을 최대공약수로 나눈 값)
+        gcm = n * m * gcd; // 위 풀이를 연산하면 이 풀이와 사실상 똑같음
         System.out.println(gcm);
     }
 
