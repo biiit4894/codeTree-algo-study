adedf9d8d5baae1062ca7914ce2e63ccf97c847f
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-11-30 23:06:36 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 129ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 241130/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 241130/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isContinuousPartialSequence(int[] arr1, int[] arr2) {
+        boolean isPartialSequence = false;
         for(int i = 0; i < arr2.length - 1; i++) {
             for(int j = 0; j < arr1.length; j++) {
+                if(arr2[i] == arr1[j]) {
+                    isPartialSequence = true;
+                }
                 if(arr2[i] == arr1[j] && arr2[i+1] != arr1[j+1]) {
                     return false;
                 }
+            }
+            if(!isPartialSequence) {
+                return false;
             }
         }
         return true;
