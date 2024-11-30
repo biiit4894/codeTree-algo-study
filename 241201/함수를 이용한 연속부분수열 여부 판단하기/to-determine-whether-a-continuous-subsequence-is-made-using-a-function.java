e968d55f288667838ada8756c9aa05bbfa27c18b
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-01 00:51:31 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241201/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+    public static boolean isContinuousPartialSequence(int[] arr1, int[] arr2) {
+        boolean isPartialSequence = false;
+
+        for(int i = 0; i < arr2.length - 1; i++) {
+            for(int j = 0; j < arr1.length; j++) {
+                if(arr2[i] == arr1[j] {
+                    isPartialSequence = true;
+                })
+                if(arr2[i] == arr1[j] && arr2[i+1] != arr1[j+1]) {
+                    return false;
+                }
+            }
+        }
+        if(!isPartialSequence) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int[] arr1 = new int[n1];
+        for(int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        int[] arr2 = new int[n2];
+        for(int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        if(isContinuousPartialSequence(arr1, arr2)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
