699889c1dea77fa905bf110fc54e1140236a4c9c
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-01 15:18:08 +0900

    Title: 그 계절, 그 날, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241201/그 계절, 그 날/that-season-that-day.java
+++ 241201/그 계절, 그 날/that-season-that-day.java
@@ -2,60 +2,57 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int y, m, d;
+    public static boolean isLeapYear(int y) {
+        // 4의 배수가 아니라면 윤년이 확실히 아님.
+        if(y % 4 != 0) {
+            return false;
+        }
 
-    public static boolean judgeLeapYear() {
-        
-        // 문제에서 주어진 윤년의 조건을 그대로 옮기면 될까? 잘 생각해보자
+        // 여기까지 온 이상 4의 배수임을 가정해도 됨
+        // 그 중 100의 배수가 아니라면 확실히 윤년
+        if(y % 100 != 0) {
+            return true;
+        }
+
+        // 여기까지 온 이상 100의 배수임을 가정해도 됨
+        // 그 중 400의 배수라면 확실히 윤년
         if(y % 400 == 0) {
             return true;
         }
-        if(y % 100 == 0) {
-            return false;
-        }
-        if(y % 4 == 0) {
-            return true;
-        }
+
+        // 여기까지 온 이상 100의 배수이지만 400의 배수가 아님
+        // 따라서 확실히 윤년이 아님
         return false;
     }
 
-    public static int findDays() {
-        if (m == 4 || m == 6 || m == 9 || m == 11) {
-            return 30;
-        } 
-        if(judgeLeapYear()) {
-            if(m == 2) {
-                return 29;
-            }
-        } else if(m == 2) {
-            return 28;
-        } 
-        return 31;
+    public static boolean isExistDay(int y, int m, int d) {
+        int[] numOfDays = new int[]{0, 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 0~12월
 
+        numOfDays[2] = isLeapYear(y) ? 29 : 28;
+
+        return d <= numOfDays[m];
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        y = sc.nextInt();
-        m = sc.nextInt();
-        d = sc.nextInt();
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
 
-        if(d <= findDays()) {
-            if(m >= 3 && m <= 5) {
-                System.out.println("Spring");
-            }
-            if(m >= 6 && m <= 8) {
-                System.out.println("Summer");
+        // y년 m월 d일이 존재하지 않는다면 -1을 출력
+        if(!isExistDay(y, m ,d)) {
+            System.out.println("-1");
+        }
 
-            }
-            if(m >= 9 && m <= 11) {
-                System.out.println("Fall");
-            }
-            if(m == 12 || m <= 2) {
-                System.out.println("Winter");
-            }
+        // y년 m월 d일이 존재한다면 달에 맞는 계절을 출력
+        if(3 <= m && m <= 5) {
+            System.out.print("Spring");
+        } else if (6 <= m && m <= 8) {
+            System.out.print("Summer");
+        } else if(9 <= m && m <= 11) {
+            System.out.print("Fall");
         } else {
-            System.out.println(-1);
+            System.out.print("Winter");
         }
     }
 }
