8b9f070423a979c3f0f8b0d4afca09455abb6684
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-05 03:08:05 +0900

    Title: 중앙값 계산 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241205/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static int MAX_INT = 100;
+    public static int n;
+    public static int[] arr = new int[MAX_INT];
+
+    public static void findMedian() {
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0) {
+                if(i == 1) {
+                    System.out.print(arr[i] + " ");
+                } else {
+                    System.out.print(arr[(i + 1) / 2] + " ");
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, 0, n);
+        findMedian();
+    }
+}
