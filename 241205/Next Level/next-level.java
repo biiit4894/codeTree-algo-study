b707f82ad80e808e244a6ac5e707a83b87345aae
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-05 03:39:09 +0900

    Title: Next Level, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241205/Next Level/next-level.java
+++ 241205/Next Level/next-level.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 class Level {
     String id;
-    String level;
+    int level;
 
     public Level() {
-        this.id = "codetree";
-        this.level = "10";
+        this.id = "";
+        this.level = 0;
     }
 
-    public Level(String id, String level) {
+    public Level(String id, int level) {
         this.id = id;
         this.level = level;
     }
@@ -19,10 +19,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String id = sc.next();
-        String level = sc.next();
+        int level = sc.next();
 
         Level level1 = new Level();
-        Level level2 = new Level(id, level);
+        level1.id = "codetree";
+        level1.level = 10;
+
+        Level level2 = new Level();
+        level2.id = id;
+        level2.level = level;
 
         System.out.println("user " + level1.id + " lv " + level1.level);
         System.out.println("user " + level2.id + " lv " + level2.level);
