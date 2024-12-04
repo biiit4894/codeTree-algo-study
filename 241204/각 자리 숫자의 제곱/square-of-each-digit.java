742b9ffa467fa9cad8e048dec84f6f4b35e5519f
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-04 14:34:23 +0900

    Title: 각 자리 숫자의 제곱, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241204/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int getSquareSum(int n) {
+        if(n < 10) {
+            return n * n;
+        }
+        return getSquareSum(n / 10) + (n % 10) * (n % 10);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(getSquareSum(n));
+    }
+}
