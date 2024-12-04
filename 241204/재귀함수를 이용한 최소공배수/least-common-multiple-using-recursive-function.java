69c4c3550d98f4d0d69d2f96b50de5bbbdd66fbb
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-04 20:01:38 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 106ms, Memory: 10MB, Status: Passed - CodeTree

--- 241204/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 241204/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[] arr;
+    public static final int MAX_N = 10;
+
+    public static int[] arr = new int[MAX_N + 1];
     public static int n;
 
     // 두 수간의 최소공배수를 구하기
-    public static int getLeastCommonMultiple(int a, int b) {
+    public static int lcm(int a, int b) {
         int gcd = 1; // 최소공배수, 최대공약수
         for(int j = 1; j <= Math.min(a, b); j++) {
             if(a % j == 0 && b % j == 0) {
@@ -16,22 +18,29 @@ public class Main {
         return a * b / gcd;
     }
 
-    // 
-    public static int getLeastCommonMultipleAll(int index) {
+    // index번째까지 인덱스의 숫자 중에서 가장 큰 값을 반환
+    public static int getLCMAll(int index) {
+
+        // 남은 원소가 1개라면 그 자신이 답이 된다
         if(index == 1) {
             return arr[1];
         }
-        return getLeastCommonMultiple(getLeastCommonMultipleAll(index - 1), arr[index]);
+
+        // 1번째 ~ index - 1번째 원소의 최소공배수를 구한 결과와
+        // 현재 index 원소의 최소공배수를 구해 반환한다.
+        return lcm(getLCMAll(index - 1), arr[index]);
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new int[n + 1];
+
         for(int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println(getLeastCommonMultipleAll(n));
+
+        // 모든 수의 최소공배수를 구한다.
+        System.out.println(getLCMAll(n));
 
     }
 }
