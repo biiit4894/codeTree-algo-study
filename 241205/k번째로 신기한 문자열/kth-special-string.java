11aedf1c9d80e5c2fbb64c65db5f234d9537687b
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-05 02:55:19 +0900

    Title: k번째로 신기한 문자열, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 241205/k번째로 신기한 문자열/kth-special-string.java
+++ 241205/k번째로 신기한 문자열/kth-special-string.java
@@ -1,28 +1,51 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.ArrayList;
+
+// 해설
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        String t = sc.next();
-        String[] arr = new String[n];
+    public static final int MAX_N = 100;
+    public static int n, k;
+    public static String t;
+    public static int cnt;
 
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.next();
+    public static String[] str = new String[MAX_N];
+    public static String[] words = new String[MAX_N];
+
+    // a가 b로 시작하는지를 확인하기(startsWith를 직접 구현)
+    public static boolean startsWith(String a, String b) {
+        // b의 길이가 더 길 수는 없음
+        if(a.length() < b.length()) {
+            return false;
         }
-        Arrays.sort(arr);
 
-
-        ArrayList<String> arr2 = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            if(arr[i].startsWith(t)) {
-                arr2.add(arr[i]);
+        // b의 길이만큼 살펴보며, a와 문자열이 완전히 동일한지 확인
+        for(int i = 0; i < b.length(); i++) {
+            if(a.charAt(i) != b.charAt(i)) {
+                return false;
             }
         }
-        System.out.println(arr2.get(k - 1));
+
+        return true;
+    } 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        t = sc.next();
+
+        for(int i = 0; i < n; i++) {
+            str[i] = sc.next();
+
+            if(startsWith(str[i], t)) {
+                words[cnt++] = str[i];
+            }
+        }
+
+        Arrays.sort(words, 0, cnt);
+
+        System.out.println(words[k - 1]);
 
         
     }
