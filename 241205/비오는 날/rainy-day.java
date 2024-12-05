c438a0f36fdffa68218c7a78e212588674f17123
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-05 22:06:39 +0900

    Title: 비오는 날, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 241205/비오는 날/rainy-day.java
+++ 241205/비오는 날/rainy-day.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-
-class ForeCast {
+// 해설
+class Forecast {
     String date;
     String day;
     String weather;
 
-    public ForeCast(String date, String day, String weather) {
+    public Forecast(String date, String day, String weather) {
         this.date = date;
         this.day = day;
         this.weather = weather;
@@ -13,52 +13,27 @@ class ForeCast {
 }
 
 public class Main {
+    public static Forecast answer = new Forecast("9999-99-99", "", "");
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        ForeCast[] forecasts = new ForeCast[n];
-
 
         for(int i = 0; i < n; i++) {
             String date = sc.next();
             String day = sc.next();
             String weather = sc.next();
-            forecasts[i] = new ForeCast(date, day, weather);
-        }
 
-        int rainIdx = 0;
-        for(int i = 0; i < n; i++) {
-
-            String currentWeather = forecasts[rainIdx].weather;
-            String targetWeather = forecasts[i].weather;
-
-            int currentYear = Integer.parseInt(forecasts[rainIdx].date.substring(0, 4));
-            int targetYear = Integer.parseInt(forecasts[i].date.substring(0, 4));
-            
-            int currentMonth = Integer.parseInt(forecasts[rainIdx].date.substring(5, 7));
-            int targetMonth = Integer.parseInt(forecasts[i].date.substring(5, 7));
-
-            int currentDay = Integer.parseInt(forecasts[rainIdx].date.substring(8));
-            int targetDay = Integer.parseInt(forecasts[i].date.substring(8));
-
-            if(!currentWeather.equals("Rain") && targetWeather.equals("Rain")) {
-                rainIdx = i;
-            } else if(currentWeather.equals("Rain") && targetWeather.equals("Rain")) {
-                if(currentYear > targetYear) {
-                    rainIdx = i;
-                } else if(currentYear == targetYear) {
-                    if(currentMonth > targetMonth) {
-                        rainIdx = i;
-                    } else if(currentMonth == targetMonth) {
-                        if(currentDay > targetDay) {
-                            rainIdx = i;                     
-                        }
-                    }
+            Forecast f = new Forecast(date, day, weather);
+            if(weather.equals("Rain")) {
+                // 비가 오는 경우 가장 최근인지 확인하고
+                // 가장 최근일 경우 정답을 업데이트한다.
+                if(answer.date.compareTo(f.date) > 0) {
+                    answer = f;
                 }
-            } 
-            
+            }
         }
-        System.out.println(forecasts[rainIdx].date + " " + forecasts[rainIdx].day + " " + forecasts[rainIdx].weather);
+
+        System.out.println(answer.date + " " + answer.day + " " + answer.weather);
     }
 }
