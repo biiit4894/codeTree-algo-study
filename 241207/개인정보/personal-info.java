dc076c8e121237145173f90aeaef833ea3327cfd
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-07 06:04:54 +0900

    Title: 개인정보, Time: 152ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241207/개인정보/personal-info.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Person {
+    String name;
+    int height;
+    double weight;
+
+    public Person(String name, int height, double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+} 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Person[] people = new Person[5];
+        for(int i = 0 ; i < 5; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            double weight = sc.nextDouble();
+            people[i] = new Person(name, height, weight);
+        }
+
+        Arrays.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person person1, Person person2) {
+                return person1.name.compareTo(person2.name);
+            }
+        });
+
+        System.out.println("name");
+        for(int i = 0; i < 5; i++) {
+            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight);
+        }
+
+        Arrays.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person person1, Person person2) {
+                return person2.height - person1.height;
+            }
+        });
+        
+        System.out.println();
+
+        System.out.println("height");
+        for(int i = 0; i < 5; i++) {
+            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight);
+        }
+
+    }
+}
