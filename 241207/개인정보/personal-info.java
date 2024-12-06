667d3bf5b3a168e3a33a7d9095e063fd28158357
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-07 06:07:19 +0900

    Title: 개인정보, Time: 167ms, Memory: 12MB, Status: Passed - CodeTree

--- 241207/개인정보/personal-info.java
+++ 241207/개인정보/personal-info.java
@@ -28,13 +28,13 @@ public class Main {
         Arrays.sort(people, new Comparator<Person>() {
             @Override
             public int compare(Person person1, Person person2) {
-                return person1.name.compareTo(person2.name);
+                return person1.name.compareTo(person2.name); // String.compareTo() 메서드로 사전식 순서로 문자열을 비교해 정렬해야 함
             }
         });
 
         System.out.println("name");
         for(int i = 0; i < 5; i++) {
-            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight);
+            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight); // %.1f까지 사용하면 완벽할 것
         }
 
         Arrays.sort(people, new Comparator<Person>() {
@@ -48,7 +48,7 @@ public class Main {
 
         System.out.println("height");
         for(int i = 0; i < 5; i++) {
-            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight);
+            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight); // %.1f까지 사용하면 완벽할 것
         }
 
     }
