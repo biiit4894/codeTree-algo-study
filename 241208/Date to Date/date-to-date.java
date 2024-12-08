552f35c4b57b84370f5a43831e3f35ca5b8e2958
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 10:53:22 +0900

    Title: Date to Date, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241208/Date to Date/date-to-date.java
+++ 241208/Date to Date/date-to-date.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
-// 간단한 풀이 - 해설
+// while문 풀이
 public class Main {
-    public static int numOfDays(int m, int d) {
-        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
-        int totalDays = 0;
-
-        for(int i = 1; i < m; i++) {
-            totalDays += days[i];
-        }
-        totalDays += d;
-
-        return totalDays;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+
         int m1, d1, m2, d2;
+        int elapsedDays = 0;
 
         m1 = sc.nextInt();
         d1 = sc.nextInt();
         m2 = sc.nextInt();
         d2 = sc.nextInt();   
 
-        System.out.println(numOfDays(m2, d2) - numOfDays(m1, d1) + 1);
+        while(true) {
+            if(m1 == m2 && d1 == d2) {
+                break;
+            }
+            elapsedDays++;
+            d1++;
+
+            if(d1 > days[m1]) {
+                d1 = 0;
+                m1++;
+            }
+        }
+
+        System.out.println(elapsedDays);
     }
 }
