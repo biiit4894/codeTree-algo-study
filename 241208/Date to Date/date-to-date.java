d98c1d5bb1774651348adb4e4eb3fbe185a143c8
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 10:42:22 +0900

    Title: Date to Date, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241208/Date to Date/date-to-date.java
+++ 241208/Date to Date/date-to-date.java
@@ -12,20 +12,23 @@ public class Main {
         m2 = sc.nextInt();
         d2 = sc.nextInt();   
 
-        int endDate = 0;
-        for(int i = 1; i <= m2; i++) {
-            endDate += num_of_days[i];
+        int endDate = 0, startDate = 0;
+
+        if(m1 == m2 && d1 == d2) {
+            System.out.println(1);
+        } else {
+            for(int i = 1; i <= m2; i++) {
+                endDate += num_of_days[i];
+            }
+            endDate += d2;
+
+            for(int i = 1; i <= m1; i++) {
+                startDate += num_of_days[i];
+            }
+            startDate += d1;
+
+            System.out.println(endDate - startDate);
         }
-        endDate += d2;
-
-        int startDate = 0;
-        for(int i = 1; i <= m1; i++) {
-            startDate += num_of_days[i];
-        }
-        startDate += d1;
-
-        System.out.println(endDate - startDate);
-
-        
+      
     }
 }
