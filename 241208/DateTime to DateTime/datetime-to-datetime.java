2e95ac47da0c35960d6e40fab109ab07d1739720
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 11:00:27 +0900

    Title: DateTime to DateTime, Time: 3053ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 241208/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = 11, hour = 11, mins = 11;
+
+        int a = sc.nextInt(); // 일
+        int b = sc.nextInt(); // 시
+        int c = sc.nextInt(); // 분
+
+        int elapsedTime = 0;
+        while(true) {
+            if(day == a && hour == b && mins == c) {
+                break;
+            }
+
+            elapsedTime++;
+            mins++;
+
+            if(mins == 60) {
+                mins = 0;
+                hour++;
+            }
+            if(hour == 24) {
+                hour = 0;
+                day++;
+            }
+        }
+        System.out.println(elapsedTime);
+
+
+        
+    }
+}
