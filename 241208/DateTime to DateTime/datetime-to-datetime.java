3f9e477d42b1c5be7a2af6d6eb71e2389a662ae0
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 11:05:48 +0900

    Title: DateTime to DateTime, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241208/DateTime to DateTime/datetime-to-datetime.java
+++ 241208/DateTime to DateTime/datetime-to-datetime.java
@@ -11,31 +11,34 @@ public class Main {
 
         int elapsedTime = 0;
 
-        if(day > a || hour > b || mins > c) {
+        if(day > a) {
+            elapsedTime = -1;
+        } else if(day == a && (hour > b) ) {
+            elapsedTime = -1;
+        } else if(hour == b && (mins > c)) {
             elapsedTime = -1;
         } else {
+
             while(true) {
-            if(day == a && hour == b && mins == c) {
-                break;
-            }
+                if(day == a && hour == b && mins == c) {
+                    break;
+                }
 
-            elapsedTime++;
-            mins++;
+                elapsedTime++;
+                mins++;
 
-            if(mins == 60) {
-                mins = 0;
-                hour++;
+                if(mins == 60) {
+                    mins = 0;
+                    hour++;
+                }
+                if(hour == 24) {
+                    hour = 0;
+                    day++;
+                }
             }
-            if(hour == 24) {
-                hour = 0;
-                day++;
-            }
-        }
         }
         
         System.out.println(elapsedTime);
 
-
-        
     }
 }
