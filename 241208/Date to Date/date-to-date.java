176b790209206f1c4fc0e2e60bdc45e48df58c20
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 10:49:35 +0900

    Title: Date to Date, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

--- 241208/Date to Date/date-to-date.java
+++ 241208/Date to Date/date-to-date.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
-// 간단한 풀이
+// 간단한 풀이 - 해설
 public class Main {
+    public static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+        int totalDays = 0;
+
+        for(int i = 1; i < m; i++) {
+            totalDays += days[i];
+        }
+        totalDays += d;
+
+        return totalDays;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
 
         int m1, d1, m2, d2;
 
@@ -12,20 +23,6 @@ public class Main {
         m2 = sc.nextInt();
         d2 = sc.nextInt();   
 
-        int endDate = 0, startDate = 0;
-        
-        for(int i = 1; i < m2; i++) {
-            endDate += num_of_days[i];
-        }
-        endDate += d2;
-
-        for(int i = 1; i < m1; i++) {
-            startDate += num_of_days[i];
-        }
-        startDate += d1;
-
-        System.out.println(endDate - startDate + 1);
-
-        
+        System.out.println(numOfDays(m2, d2) - numOfDays(m1, d1) + 1);
     }
 }
