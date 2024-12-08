6da4038d032e0c6c8047617ad0ae9d0251b79bf9
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 09:23:23 +0900

    Title: 원점으로부터의 거리, Time: 259ms, Memory: 14MB, Status: Passed - CodeTree

--- 241208/원점으로부터의 거리/distance-from-origin.java
+++ 241208/원점으로부터의 거리/distance-from-origin.java
@@ -1,42 +1,47 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
+// 해설 : 좌표 모두 저장 없이 맨해튼 거리만 저장해도 괜찮음
 class Point implements Comparable<Point> {
-    int num, x, y;
-    public Point(int num, int x, int y) {
-        this.num = num;
-        this.x = x;
-        this.y = y;
+    int dist;
+    int index;
+
+    public Point(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
     }
 
     @Override
     public int compareTo(Point point) {
-        int distance1 = Math.abs(this.x) + Math.abs(this.y);
-        int distance2 = Math.abs(point.x) + Math.abs(point.y);
-        if(distance1 != distance2) {
-            return distance1 - distance2;
+        if(dist != point.dist) {
+            return dist - point.dist;
         } 
-        return this.num - point.num;
+        return index - point.index;
     }
 }
 
 public class Main {
+
+    // 원점과의 거리를 계산하는 함수
+    public static int getDistFromOrigin(int x, int y) {
+        return Math.abs(x) + Math.abs(y);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Point[] points = new Point[n];
 
         for(int i = 0; i < n; i++) {
-            int num = i + 1;
             int x = sc.nextInt();
             int y = sc.nextInt();
-            points[i] = new Point(num, x, y);
+            // 원점과의 거리와 index를 저장
+            points[i] = new Point(getDistFromOrigin(x, y), i + 1);
         }
 
         Arrays.sort(points, 0, n);
 
         for(int i = 0; i < n; i++) {
-            System.out.println(points[i].num);
+            System.out.println(points[i].index);
         }
 
     }
