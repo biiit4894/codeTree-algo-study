e2e592822e415d0feb1141cc8abc23f2806b68ad
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 11:14:19 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241208/DateTime to DateTime/datetime-to-datetime.java
+++ 241208/DateTime to DateTime/datetime-to-datetime.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// 더 간단한 풀이
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,36 +9,14 @@ public class Main {
         int b = sc.nextInt(); // 시
         int c = sc.nextInt(); // 분
 
-        int elapsedTime = 0;
+        int endTime = (a - 1) * 24 * 60 + b * 60 + c;
+        int startTime = (day - 1) * 24 * 60 + hour * 60 + mins;
 
-        if(day > a) {
-            elapsedTime = -1;
-        } else if(day == a && (hour > b) ) {
-            elapsedTime = -1;
-        } else if(hour == b && (mins > c)) {
-            elapsedTime = -1;
+        if(starTime > endTime) {
+            System.out.println(-1);
         } else {
-
-            while(true) {
-                if(day == a && hour == b && mins == c) {
-                    break;
-                }
-
-                elapsedTime++;
-                mins++;
-
-                if(mins == 60) {
-                    mins = 0;
-                    hour++;
-                }
-                if(hour == 24) {
-                    hour = 0;
-                    day++;
-                }
-            }
+            System.out.println(endTime - startTime);
         }
-        
-        System.out.println(elapsedTime);
 
     }
 }
