83283b48e55de8a2818ea374ed0dceede4d01f37
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-08 13:36:26 +0900

    Title: 그 요일은, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241208/그 요일은/the-day-of-the-day.java
+++ 241208/그 요일은/the-day-of-the-day.java
@@ -1,44 +1,47 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static int getTotalDays(int m, int d) {
-        int[] days_of_month = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int totalDays = 0;
-
-        for(int i = 1; i < m - 1; i++) {
-            totalDays += days_of_month[i];
-        }
-        totalDays += d;
-        return totalDays;
-    }
+    public static int[] days = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    public static String[] week = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        String day = sc.next();
+        String str = sc.next();
 
-        String[] days = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        
-        int endDate = getTotalDays(m2, d2);
-        int startDate = getTotalDays(m1, d1);
-        int diff = endDate - startDate;
+        int total = countDay(m2,d2)-countDay(m1,d1);
+        int index = findIndex(str);
+        System.out.print(countWeek(index, total));
+    }
 
-        int dayIdx = 0;
-        for(int i = 0; i < 7; i++) {
-            if(day.equals(days[i])) {
-                dayIdx = i;
+    public static int countDay(int Month, int Day){
+        int sum = 0;
+
+        for(int i=1; i<Month; i++){
+            sum += days[i];
+        }
+        return (sum+Day);
+    }
+
+    public static int findIndex(String temp){
+        int t = -1;
+        for(int i=0; i<7; i++){
+            if(temp.equals(week[i])){
+                t=i;
             }
         }
+        return t;
+    }
 
-
-        if(diff % 7 >= dayIdx) {
-            System.out.println(diff / 7 + 1);
-        } else {
-            System.out.println(diff / 7);
+    public static int countWeek(int index, int total){
+        int t = total/7;
+        if(total%7 >= index){
+            t++;
         }
-
+        return t;
     }
 }
