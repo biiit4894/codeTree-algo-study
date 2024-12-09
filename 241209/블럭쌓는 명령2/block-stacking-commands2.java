8fd7ef98c9e058774268adca1e5201fef1c74337
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-09 15:23:40 +0900

    Title: 블럭쌓는 명령2, Time: 128ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241209/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        int k = sc.nextInt();
+        int max = 0;
+
+        for(int i = 0; i < k; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int j = a ; j <= b; j++) {
+                arr[j]++;
+                if(max < arr[j]) {
+                    max = arr[j];
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
