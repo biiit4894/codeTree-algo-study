2c8e1973278ffef7a62b6b89bc1b86891a931342
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-09 15:24:49 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241209/블럭쌓는 명령2/block-stacking-commands2.java
+++ 241209/블럭쌓는 명령2/block-stacking-commands2.java
@@ -7,16 +7,20 @@ public class Main {
         int[] arr = new int[n + 1];
 
         int k = sc.nextInt();
-        int max = 0;
 
         for(int i = 0; i < k; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             for(int j = a ; j <= b; j++) {
                 arr[j]++;
-                if(max < arr[j]) {
-                    max = arr[j];
-                }
+                
+            }
+        }
+
+        int max = 0;
+        for(int i = 1; i <= n; i++) {
+            if(max < arr[j]) {
+                max = arr[j];
             }
         }
 
