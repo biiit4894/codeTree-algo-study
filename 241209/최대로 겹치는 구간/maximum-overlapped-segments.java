910892b1142c7c22e2c8184d809185f81091698e
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-09 16:22:23 +0900

    Title: 최대로 겹치는 구간, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241209/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241209/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,26 +1,46 @@
 import java.util.Scanner;
-
+// 해설
 public class Main {
-    public static int MAX_INT = 201;
-    public static int OFFSET = 100;
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
+
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_R];
+
+    public static int[] checked = new int[MAX_R + 1];
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[MAX_INT];
+
+        n = sc.nextInt();
+
         for(int i = 0; i < n; i++) {
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            for(int j = x1; j <= x2 - 1; j++) {
-                arr[j + OFFSET]++;
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
+        }
+        
+        // 구간 칠하기
+        // 구간 단위로 진행하는 문제이기 때문에
+        // x2[i]에 등호가 들어가지 않음 !!
+        for(int i = 0; i < n; i++) {
+            for(int j = x1[i]; j < x2[i]; j++) {
+                checked[j]++;
             }
         }
+
+        // 최댓값 구하기
         int max = 0;
-        for(int i = -OFFSET; i <= OFFSET; i++) {
-            if(max < arr[i + OFFSET]) {
-                max = arr[i + OFFSET];
+        for(int i = 0; i <= MAX_R; i++) {
+            if(checked[i] > max) {
+                max = checked[i];
             }
         }
-        System.out.println(max);
+
+        System.out.print(max);
     }
 }
