29f4640d7b12508f9d5c8a770e9d82fe230d7ddc
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-17 17:29:58 +0900

    Title: 흰검 칠하기, Time: 219ms, Memory: 17MB, Status: Passed - CodeTree

--- 241217/흰검 칠하기/painting-white-black.java
+++ 241217/흰검 칠하기/painting-white-black.java
@@ -1,56 +1,50 @@
 import java.util.Scanner;
-
+// 해설
 public class Main {
-    public static final int MAX_R = 100000;
+    public static final int MAX_K = 100000;
     
-    public static int[] whiteCount = new int[2 * MAX_R + 1];
-    public static int[] blackCount = new int[2 * MAX_R + 1];
-
-    public static final char[] checked = new char[2 * MAX_R + 1];
+    public static int n;
+    public static int[] a = new int[2 * MAX_K + 1];
+    public static int[] cntB = new int[2 * MAX_K + 1];
+    public static int[] cntW = new int[2 * MAX_K + 1];
+    public static int b, w, g;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // 변수 입력
+        n = sc.nextInt();
 
-        int cur = MAX_R;
-        for(int i = 0; i < n; i++) {
-            int dis = sc.nextInt();
-            char dir = sc.next().charAt(0);
-
-            if(dir == 'L') {
-                while(dis > 0) {
-                    checked[cur] = 'w';
-                    whiteCount[cur]++;
-                    cur--;
-                    dis--;
-                } 
-                cur++; // 마지막 위치 보정
-            } else {                
-                while(dis > 0) {
-                    checked[cur] = 'b';
-                    blackCount[cur]++;
-                    cur++;
-                    dis--;
+        int cur = MAX_K;
+        for(int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+            if(c == 'L') {
+                // x칸 왼쪽으로 칠합니다.
+                while(x-- > 0) {
+                    a[cur] = 1;
+                    cntW[cur]++;
+                    if(x > 0) cur--;
                 }
-                cur--; // 마지막 위치 보정
             }
-            
-        }
-
-        int white = 0;
-        int black = 0;
-        int gray = 0;
-    
-        for(int i = 0; i < 2 * MAX_R + 1; i++) {
-            if(whiteCount[i] >= 2 && blackCount[i] >= 2) {
-                gray++;
-            } else if(checked[i] == 'w') {
-                white++;
-            } else if(checked[i] == 'b') {
-                black++;
+            else {
+                // x칸 오른쪽으로 칠합니다.
+                while(x-- > 0) {
+                    a[cur] = 2;
+                    cntB[cur]++;
+                    if(x > 0) cur++;
+                }
             }
         }
 
-        System.out.print(white + " " + black + " " + gray);
+        for(int i = 0; i <= 2 * MAX_K; i++) {
+            // 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+            if(cntB[i] >= 2 && cntW[i] >= 2) g++;
+            // 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+            else if(a[i] == 1) w++;
+            else if(a[i] == 2) b++;
+        }
+
+        // 정답을 출력합니다.
+        System.out.print(w + " " + b + " " + g);
     }
 }
