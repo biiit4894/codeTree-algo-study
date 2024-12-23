d7a1bb3906d641bdcc9ba2992cf7562666adece0
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2024-12-24 05:21:40 +0900

    Title: 벌금은 누구에게, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241224/벌금은 누구에게/who-will-pay.java
+++ 241224/벌금은 누구에게/who-will-pay.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static int n, m, k;
 
+    public static int getPunishStudentNo(int[] nums) {
+        for(int i = 1; i <= n; i++) {
+            if(nums[i] >= k) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -11,16 +20,13 @@ public class Main {
         int[] arr = new int[m]; // 벌칙을 받은 학생들의 번호
         int[] nums = new int[n + 1]; // 학생 번호별로 벌칙을 받은 횟수
 
+        boolean flag = false; // 벌칙을 받는 학생이 존재하는지 여부
         for(int i = 0; i < m; i++) {
             arr[i] = sc.nextInt();
             nums[arr[i]]++;
-        }
-
-        boolean flag = false; // 벌금을 내는 학생이 존재하는지 여부
-        for(int i = 1; i <= n; i++) {
-            if(nums[i] >= k) {
+            if(getPunishStudentNo(nums) != 0) {
                 flag = true;
-                System.out.println(i);
+                System.out.println(getPunishStudentNo(nums));
                 break;
             }
         }
