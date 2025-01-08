61f01323e4d9e1d402fb07b5eb112479fac0832b
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2025-01-08 23:32:59 +0900

    Title: 좌우로 움직이는 로봇, Time: 631ms, Memory: 42MB, Status: Runtime Error - CodeTree

---
+++ 250108/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_POS = 1000000;
+    public static int[] pos_A = new int[MAX_POS];
+    public static int[] pos_B = new int[MAX_POS];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int time_A = 1;
+        for(int i = 0; i < n; i++) {
+            
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            
+            if(d == 'L') {
+                for(int j = t; j > 0; j--) {
+                    pos_A[time_A] = pos_A[time_A - 1] - 1;
+                    time_A++;
+                }
+            } else if(d == 'R') {
+                for(int j = t; j > 0; j--) {
+                    pos_A[time_A] = pos_A[time_A - 1] + 1;
+                    time_A++;
+                }
+            }
+        }
+
+        for(int i = time_A; i < MAX_POS; i++) {
+            pos_A[i] = pos_A[i - 1];
+        }
+
+        int time_B = 1;
+        for(int i = 0; i < m; i++) {
+            
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            if(d == 'L') {
+                for(int j = t; j > 0; j--) {
+                    pos_B[time_B] = pos_B[time_B - 1] - 1;
+                    time_B++;
+                }
+
+            } else if(d == 'R') {    
+                for(int j = t; j > 0; j--) {
+                    pos_B[time_B] = pos_B[time_B - 1] + 1;
+                    time_B++;
+                }
+
+            }
+        }
+
+        for(int i = time_B; i < MAX_POS; i++) {
+            pos_B[i] = pos_B[i - 1];
+        }
+
+        int answer = 0;
+        for(int i = 1; i < MAX_POS; i++) {
+            if(pos_A[i] == pos_B[i] && pos_A[i - 1] != pos_B[i- 1]) {
+                answer++;
+            }
+        }
+
+        System.out.print(answer);
+
+    }
+}
