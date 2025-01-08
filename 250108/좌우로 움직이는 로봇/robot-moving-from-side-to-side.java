30cbc2438b24f44319f5ead989a60f81a0e94265
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2025-01-08 23:34:19 +0900

    Title: 좌우로 움직이는 로봇, Time: 847ms, Memory: 58MB, Status: Passed - CodeTree

--- 250108/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250108/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static final int MAX_POS = 1000000;
-    public static int[] pos_A = new int[MAX_POS];
-    public static int[] pos_B = new int[MAX_POS];
+    public static int[] pos_A = new int[MAX_POS + 1];
+    public static int[] pos_B = new int[MAX_POS + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
