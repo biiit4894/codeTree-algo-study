8b6be6fb91fc83da5170599cf222855f5a4d0da9
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2025-01-12 14:14:28 +0900

    Title: 되돌아오기, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 250112/되돌아오기/come-back.java
+++ 250112/되돌아오기/come-back.java
@@ -1,63 +1,71 @@
 import java.util.Scanner;
-
+// 해설 풀이
 public class Main {
-    public static int n;
+    public static final int DIR_NUM = 4;
 
-    public static final int[] dx = new int[]{0, 1, 0, -1};
-    public static final int[] dy = new int[]{1, 0, -1, 0};
+    public static int n, x, y;
 
-    public static final int MAX_WIDTH = 2001;
-    public static final int OFFSET = 1000;
+    // 동, 서, 남, 북 순으로 dx, dy를 정의합니다.
+    public static int[] dx = new int[]{1, -1,  0, 0};
+    public static int[] dy = new int[]{0,  0, -1, 1};
 
-    public static int[][] board = new int[MAX_WIDTH][MAX_WIDTH];
+    // 답을 저장합니다.
+    public static int ans = -1;
 
-    public static int x = OFFSET, y = OFFSET;
+    // 지금까지 걸린 시간을 기록합니다.
+    public static int elapsedTime;
 
-    public static int getDirNum(char c) {
-        int dirNum;
-        if(c == 'E') {
-            dirNum = 0;
-        } else if(c == 'S') {
-            dirNum = 1;
-        } else if(c == 'W') {
-            dirNum = 2;
-        } else {
-            dirNum = 3;
+    // dir 방향으로 dist 만큼 이동하는 함수입니다.
+    // 만약 시작지에 도달하면 true를 반환합니다.
+    public static boolean move(int dir, int dist) {
+        while(dist-- > 0) {
+            x += dx[dir];
+            y += dy[dir];
+
+            // 이동한 시간을 기록합니다.
+            elapsedTime++;
+
+            // 시작지로 다시 돌아오면,
+            // 답을 갱신해줍니다.
+            if(x == 0 && y == 0) {
+                ans = elapsedTime;
+                return true;
+            }
         }
-        return dirNum;
-    }
-
-    public static boolean isRange(int x, int y) {
-        return (x >= 0 && x < MAX_WIDTH && y >= 0 && y < MAX_WIDTH);
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력
         n = sc.nextInt();
 
-        board[x][y] = 0;
-        int time = 0;
-        for(int i = 0; i < n; i++) {
-            char dir = sc.next().charAt(0);
-            int dis = sc.nextInt();
+        // 움직이는 것을 진행합니다.
+        while(n-- > 0) {
+            char cDir = sc.next().charAt(0);
+            int dist = sc.nextInt();
 
-            int dirNum = getDirNum(dir);
+            // 각 방향에 맞는 번호를 붙여줍니다.
+            int dir;
+            if(cDir == 'E') {
+                dir = 0;
+            } else if(cDir == 'W') {
+                dir = 1;
+            } else if(cDir == 'S') {
+                dir = 2;
+            } else {
+                dir = 3;
+            }
 
-            for(int j = 0; j < dis; j++) {
-                x += dx[dirNum];
-                y += dy[dirNum];
-                time++;
-                
-                if(isRange(x, y)) {
-                    board[x][y] = i + 1;
-                }   
+            // 주어진 방향대로 dist 만큼 위치를 이동해봅니다.
+            boolean done = move(dir, dist);
 
-                if(x == OFFSET && y == OFFSET) {
-                    System.out.println(time);
-                    return;
-                }
+            // 시작 위치에 도달했다면, 종료합니다.
+            if(done) {
+                break;
             }
         }
-        System.out.println(-1);
+        
+        System.out.println(ans);
     }
 }
