f248015fd24cb9ecd8e3a9c4c8841f58b0de4332
Hanbit Jang <82032418+biiit4894@users.noreply.github.com>
2025-01-12 14:51:15 +0900

    Title: 되돌아오기 2, Time: 200ms, Memory: 12MB, Status: Passed - CodeTree

--- 250112/되돌아오기 2/come-back-2.java
+++ 250112/되돌아오기 2/come-back-2.java
@@ -1,39 +1,49 @@
 import java.util.Scanner;
-
+// 해설 풀이
 public class Main {
-    public static int x, y;
-    public static final int answer = -1;
-    public static int elapsedTime;
-    public static String str;
+    public static final int DIR_NUM = 4;
 
-    // 북, 동, 남, 서
-    public static final int[] dx = new int[]{0, 1, 0, -1};
-    public static final int[] dy = new int[]{1, 0, -1, 0};
+    public static String dirs;
+    public static int x = 0, y = 0;
+    public static int currDir = 3;
+
+    // 동, 남, 서, 북 순으로 dx, dy를 정의한다.
+    public static final int[] dx = new int[]{1, 0, -1, 0};
+    public static final int[] dy = new int[]{0, -1, 0, 1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        str = sc.next();
+        // 입력
+        dirs = sc.next();
 
-        int dirNum = 0;
-        for(int i = 0; i < str.length(); i++) {
-            char dir = str.charAt(i);
+        // flag : 시작점으로 되돌아 왔는지 여부
+        boolean flag = false;
 
-            if(dir == 'L') {
-                dirNum = (dirNum + 3) % 4;
-            } else if(dir == 'R') {
-                dirNum = (dirNum + 1) % 4;
-            } else {
-                x += dx[dirNum];
-                y += dy[dirNum];
+        // 움직이는 것을 진행한다.
+        for(int i = 0; i < dirs.length(); i++) {
+            char cDir = dirs.charAt(i);
+
+            // 반시계방향 90' 회전
+            if(cDir == 'L') {
+                currDir = (currDir - 1 + 4) % 4;
+            } else if(cDir == 'R') { // 시계방향 90' 회전
+                currDir = (currDir + 1) % 4;
+            } else { // 직진
+                x += dx[currDir];
+                y += dy[currDir];
             }
-            elapsedTime++;
 
+            // 시작점으로 되돌아왔을 때
             if(x == 0 && y == 0) {
-                System.out.println(elapsedTime);
-                return;
+                System.out.print(i + 1);
+                flag = true;
+                break;
             }
         }
 
-        System.out.println(answer);
+        // 시작점으로 되돌아오지 못했을 때
+        if(flag == false) {
+            System.out.print(-1);
+        }
     }
 }
